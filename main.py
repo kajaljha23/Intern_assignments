33a5afd154d1f4583eda58b0ef0e22a1b9433d3e
kajaljha23 <jha.kajal2315@gmail.com>
2023-05-15 19:41:45 +0530

    committed

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from script.core.services.billing_services import item_router
 import uvicorn
+from dotenv import load_dotenv
 app = FastAPI()
 
 app.include_router(item_router)
 if __name__ == '__main__':
-    uvicorn.run(app="main:app", reload=True)
+    load_dotenv()
+    uvicorn.run(app="main:app", port=1234,reload=True)
 
