2c29db1cf2da3651f39ed54e8af589fb7a7cdf97
kajaljha23 <jha.kajal2315@gmail.com>
2023-05-19 16:48:35 +0530

    committed

--- script/utility/mongodb.py
+++ script/utility/mongodb.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pymongo import MongoClient  # import mongo client to connect
+from script.constants.app_constants import DBConstants
 
 app = FastAPI()
 
 # Creating instance of mongo client
-client = MongoClient("mongodb://intern_23:intern%40123@192.168.0.220:2717/interns_b2_23")
+client = MongoClient(DBConstants.DB_URI)
 # Creating database
-db = client["interns_b2_23"]
+db = client[DBConstants.DB_DATABASE]
 # # Creating document
-billing = db["kajalk_billing"]
+billing = db[DBConstants.DB_COllECTION]
 
 
 # creating class
